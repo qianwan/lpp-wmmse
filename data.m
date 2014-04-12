cc36fff94b8b881e9bbeff0f6b879ca8c74eb27f
Qian Wan <qianwan@ymail.com>
2014-04-12 20:18:57 +0800

    rpc

--- data.m
+++ data.m
@@ -16,12 +16,14 @@ ylabel('System Throughput (bits per channel user)');
 
 dB = [5 : 5 : 30];
 rpcWMMSE = [1, 1, 1, 1, 1, 1];
-rpcM8Q8C7 = [0.99 0.99 0.98 0.96, 0.93, 0.90];
-rpcM8Q16C7 = [0.95 0.80 0.80 0.80 0.80 0.80];
-bar1 = bar(dB, rpcWMMSE, 'BarWidth', 0.2, 'FaceColor', 'b');
+rpcM8Q8C7 = [0.99 0.99 0.98 0.96, 0.93, 0.905];
+rpcM8Q16C7 = [0.95 0.90 0.87 0.855 0.83 0.81];
+bar1 = bar(dB, rpcM8Q8C7, 'BarWidth', 0.2, 'FaceColor', 'b');
 hold on;
-bar2 = bar(dB + 0.5, rpcM8Q8C7, 'BarWidth', 0.2, 'FaceColor', 'g');
+bar2 = bar(dB - 1, rpcWMMSE, 'BarWidth', 0.2, 'FaceColor', 'k');
 hold on;
 bar3 = bar(dB + 1, rpcM8Q16C7, 'BarWidth', 0.2, 'FaceColor', 'r');
 xlabel('SNR (dB)');
 ylabel('Relative Power Consumption');
+grid on;
+legend('LPP-WMMSE, M=64', 'WMMSE', 'LPP-WMMSE, M=128');
