e1ef5afcd274928b3d11745eb8d1db4b04055c66
qianwan <qianwan@yahoo-inc.com>
2014-04-12 14:48:08 +0800

    data

--- data.m
+++ data.m
@@ -4,7 +4,13 @@ rpcM8Q8C7 = [1.0 0.99 0.99 0.98 0.99, 0.99, 0.99];
 
 rM8Q16C7 = [289, 390, 482, 596, 722, 845, 957];
 
-rM4Q5C7 = [130, 173, 216, 286, 311, 336, 342];
+rM4Q5C7 = [130, 175, 233, 286, 315, 336, 342];
 
 wmmseM4Q5C7 = [164, 213, 258, 290, 309, 315, 319];
 % power consumption 100%
+
+plot(dB, rM8Q8C7, '-s', dB, rM8Q16C7, '-o', dB, rM4Q5C7, '-^', dB, wmmseM4Q5C7, '-*');
+legend('LPP-WMMSE, M=64', 'LPP-WMMSE, M=128', 'LPP-WMMSE, M=20', 'WMMSE, M=20');
+grid on;
+xlabel('SNR (dB)');
+ylabel('System Throughput (bits per channel user)');
